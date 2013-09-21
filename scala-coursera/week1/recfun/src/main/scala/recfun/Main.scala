da006c66e1d2b558240cab930564146f1919699b
Mats Byfuglien <mats@byfuglien.net>
2013-09-22 01:25:32 +0200

    Done Ex 1 and Ex2

---
+++ scala-coursera/week1/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,48 @@
+package recfun
+import common._
+import annotation.tailrec
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  //@tailrec
+  def pascal(c: Int, r: Int): Int = {
+    if ( c == 0 || c == r ) 1 else pascal( c -1, r -1 ) + pascal(c, r -1 )
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def loop(chars: List[Char], count: Int): Int ={
+      if (chars.isEmpty || count < 0) {
+        return count
+      }
+      if ( chars.head == '(') {
+          loop( chars.tail, (count + 1))
+      }
+      else if( chars.head == ')') {
+        loop( chars.tail, count -1 )
+      }
+      else {
+        loop( chars.tail, count)
+      }
+    }
+    if (loop( chars, 0) != 0 ) false else true
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = 0
+}
